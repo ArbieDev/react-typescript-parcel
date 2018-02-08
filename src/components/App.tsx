b600000211b69670380ab831bfa4af6610cfd62f
Arbie C <acskaleidoscope@gmail.com>
2018-02-08 21:14:41 +0900

    make sure react and scss can be compiled

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
 
 class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+  }
+
+  showCurrentInput(input) {
+    this.setState({ input });
+  }
+
   render() {
     return (
       <div>
         <i>IT WORKS!</i>
+        <div>
+          <input type="text" onChange={(e) => this.showCurrentInput(e.target.value)} />
+        </div>
+        <div className="f24 blue">{this.state.input}</div>
       </div>
     );
   }
