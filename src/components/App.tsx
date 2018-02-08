4f1014a819fcd49969d7b42541c1049f81ca1ca2
Arbie C <acskaleidoscope@gmail.com>
2018-02-08 21:01:29 +0900

    git init

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+      <div>
+        <i>IT WORKS!</i>
+      </div>
+    );
+  }
+}
+
+export default App;
